1e90ea80d25a507dd17d5006897150bd914aac15
ap4ashutosh <ap4ashutosh@gmail.com>
2024-07-17 12:04:09 +0530

    lab3 installation of hashicorp/aws provider

---
+++ tf-lab3/main.tf
@@ -0,0 +1,14 @@
+# Here in this lab we will install the AWS provider
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+}
