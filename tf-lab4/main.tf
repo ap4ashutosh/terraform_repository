482f196957f1d027460281accf86cb7d50683d25
ap4ashutosh <ap4ashutosh@gmail.com>
2024-07-22 17:00:20 +0530

    completion of providers lab

--- tf-lab4/main.tf
+++ tf-lab4/main.tf
@@ -2,17 +2,31 @@
 
 terraform {
   required_providers {
+    random = {
+      source = "hashicorp/random"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 2.0"
     }
   }
 }
-
+provider "random" {
+  
+}
+# Taking the azure provider
 provider "azurerm" {
   features {}
 }
 
+resource "random_string" "name" {
+  length = 6
+  upper = false
+  lower = true
+  special = false
+}
+
+# Creating the resource group
 resource "azurerm_resource_group" "mynewrg" {
   location = "eastus"
   name     = "mynewrg"
@@ -22,6 +36,7 @@ resource "azurerm_resource_group" "mynewrg" {
 
 }
 
+# Creating a virtual network
 resource "azurerm_virtual_network" "newVnet" {
   name                = "newVnet"
   resource_group_name = azurerm_resource_group.mynewrg.name
@@ -31,25 +46,100 @@ resource "azurerm_virtual_network" "newVnet" {
   tags = {
     terraform = "True"
   }
+}
 
-  subnet {
-    name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-  subnet {
-    name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.mynewrg.name
+  virtual_network_name = azurerm_virtual_network.newVnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
+  resource_group_name  = azurerm_resource_group.mynewrg.name
+  virtual_network_name = azurerm_virtual_network.newVnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# Configuring a network interface
+resource "azurerm_network_interface" "newNIC" {
+  name                = "newNIC"
+  resource_group_name = azurerm_resource_group.mynewrg.name
+  location            = azurerm_resource_group.mynewrg.location
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Static" 
   }
 }
 
-resource "azurerm_virtual_machine" "VM1-tf" {
+resource "azurerm_public_ip" "VmIP" {
+  resource_group_name = azurerm_resource_group.mynewrg.name
+  name                = "VmIP"
+  location            = azurerm_resource_group.mynewrg.location
+  allocation_method   = "Dynamic"
+  tags = {
+    terraform = "True"
+  }
+}
+
+# Configuring a VM
+resource "azurerm_linux_virtual_machine" "newlinuxVM" {
+  name                = "newlinuxVM"
   resource_group_name = azurerm_resource_group.mynewrg.name
   location            = azurerm_resource_group.mynewrg.location
-  name                = "VM1-tf"
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
   network_interface_ids = [
-    azurerm_network_interface.nic1-tf.id
+    azurerm_network_interface.newNIC.id,
   ]
-  vm_size = "Standard_DS1_v2"
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  # os_profile{
+  #   admin_username = "admin"
+  #   admin_password = "Password1234!"
+
+  # }
+  admin_password = "Password1234!"
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  tags = {
+    terraform = "True"
+  }
+}
+
+resource "azurerm_storage_account" "new-storage"{
+  name = "azuretfstorage${random_string.name.result}"
+  resource_group_name = azurerm_resource_group.mynewrg.name
+  location = azurerm_resource_group.mynewrg.location
+  account_tier = "Standard"
+  account_replication_type = "LRS"
+  allow_nested_items_to_be_public = true
+  tags = {
+    terraform = "True"
+  }
+}
+
+resource "azurerm_storage_container" "new-container" {
+  storage_account_name = azurerm_storage_account.new-storage.name
+  name                 = "tfstate"
+  container_access_type = "private"
 }
 
 output "resource_group" {
@@ -62,3 +152,20 @@ output "subnets" {
   description = "IP range of subnets"
 }
 
+output "aboutVM_IP" {
+  value       = azurerm_linux_virtual_machine.newlinuxVM.public_ip_address
+  description = "value of public IP"
+}
+
+output "VM_password" {
+  value     = azurerm_linux_virtual_machine.newlinuxVM.admin_password
+  sensitive = true
+}
+
+output "storage_account" {
+  value = azurerm_storage_account.new-storage.access_tier
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.new-storage.name
+}
